db59ffa93e17ada97a1cc0802df41e5a785c9ab3
AlbertZ <godzaq159753@hotmail.com>
2016-10-18 10:22:10 -0400

    Logistic reg using 6 mine IDs
    Tried different time scale; too few positives

--- Analysis/src/logistic_reg.py
+++ Analysis/src/logistic_reg.py
@@ -3,8 +3,11 @@
 """Load MSHA data and run a logistic regression.
 
 @author: Albert
-@version: 0.0.1
-@date: 09/15/16
+@version: 0.0.2
+@date: 10/17/16
+
+Note:
+    data file path is relative, use your own data file path
 
 """
 
@@ -18,7 +21,9 @@ from matplotlib.colors import ListedColormap
 
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
+    """Plot decision regions for binary features.
 
+    """
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -101,7 +106,7 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
     return plt
 
 
-def test_1(data_file_path, feature_list):
+def test_1(data_file_path, resolution=0.5, *args):
     """Plot two-feature decision boundary.
 
     ``log(penalty)`` vs. ``num_violation``
@@ -113,16 +118,21 @@ def test_1(data_file_path, feature_list):
     data = pd.read_csv(data_file_path)
     data.fillna(0, inplace=True)
 
-    data[feature_list[2]] = np.log(data[feature_list[2]] + 1)
-    x_train = data[[feature_list[0], feature_list[2]]].values
-    y_train = data['accident_label_t_plus_1'].values
+    feature1 = args[0]
+    feature2 = args[1]
+    label = args[2]
+
+    data[feature2] = np.log(data[feature2] + 1)
+    x_train = data[[feature1, feature2]].values
+    y_train = data[label].values
 
     lr = LogisticRegression(C=500)
     lr.fit(x_train, y_train)
 
-    plot_decision_regions(x_train, y_train, classifier=lr, resolution=0.5)
-    plt.xlabel(feature_list[0])
-    plt.ylabel('log of ' + feature_list[2])
+    plot_decision_regions(x_train, y_train, classifier=lr,
+                          resolution=resolution)
+    plt.xlabel(feature1)
+    plt.ylabel('log of ' + feature2)
     plt.legend(loc='upper left')
 
     # plt.tight_layout()
@@ -130,7 +140,7 @@ def test_1(data_file_path, feature_list):
     plt.show()
 
 
-def test_2(data_file_path, feature_list):
+def test_2(data_file_path, resolution=0.5, *args):
     """Plot two-feature decision boundary.
 
     ``num_violation_t`` vs. ``num_violation_t_minus_1``
@@ -142,15 +152,20 @@ def test_2(data_file_path, feature_list):
     data = pd.read_csv(data_file_path)
     data.fillna(0)
 
-    x_train = data[feature_list[0:2]].values
-    y_train = data['accident_label_t_plus_1'].values
+    feature1 = args[0]
+    feature2 = args[1]
+    label = args[2]
+
+    x_train = data[[feature1, feature2]].values
+    y_train = data[label].values
 
     lr = LogisticRegression(C=500)
     lr.fit(x_train, y_train)
 
-    plot_decision_regions(x_train, y_train, classifier=lr, resolution=0.5)
-    plt.xlabel(feature_list[0])
-    plt.ylabel(feature_list[1])
+    plot_decision_regions(x_train, y_train, classifier=lr,
+                          resolution=resolution)
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
     plt.legend(loc='upper left')
 
     # plt.tight_layout()
@@ -158,7 +173,7 @@ def test_2(data_file_path, feature_list):
     plt.show()
 
 
-def test_3(data_file_path, feature_list):
+def test_3(data_file_path, *args):
     """Check whether label is reasonable.
 
     Plot ``num_violation`` vs. ``accident_label_t_plus_1``
@@ -169,44 +184,214 @@ def test_3(data_file_path, feature_list):
     """
     data = pd.read_csv(data_file_path)
 
-    acc_label = data['accident_label_t_plus_1']
-    x_val = data[feature_list[2]]
+    feature1 = args[0]
+    label = args[1]
+
+    acc_label = data[label]
+    x_val = data[feature1]
 
     plt.scatter(x_val, acc_label)
-    plt.xlabel(feature_list[2])
-    plt.ylabel('accident_label_t_plus_1')
+    plt.xlabel(feature1)
+    plt.ylabel(label)
     plt.ylim(-0.5, 1.5)
     # plt.legend(loc='upper left')
 
     plt.show()
 
 
-def test_4(data_file_path, feature_list):
+def test_4(data_file_path, randseed, *args):
     """Plot learning curve.
 
     """
     data = pd.read_csv(data_file_path)
 
-    x_train = data[feature_list[0:1]].values
-    y_train = data['accident_label_t_plus_1'].values
+    feature1 = args[0]
+    label = args[1]
+
+    x_train = data[[feature1]].values
+    y_train = data[label].values
 
     title = 'Learning Curve'
-    cv = cross_validation.ShuffleSplit(x_train.shape[0], random_state=100)
+    cv = cross_validation.ShuffleSplit(x_train.shape[0],
+                                       random_state=randseed)
     estimator = LogisticRegression(C=500)
     plot_learning_curve(estimator, title, x_train, y_train, cv=cv)
 
     plt.show()
 
 
+def test_5(data_file_path, resolution, *args):
+    """`last_yr_penal` vs `last_yr_viols`
+
+    """
+    data = pd.read_csv(data_file_path)
+    data.fillna(0)
+
+    feature1 = args[0]
+    feature2 = args[1]
+    label = args[2]
+
+    x_train = data[[feature1, feature2]].values
+    y_train = data[label].values
+
+    lr = LogisticRegression(C=500)
+    lr.fit(x_train, y_train)
+
+    plot_decision_regions(x_train, y_train, classifier=lr,
+                          resolution=resolution)
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.legend(loc='upper left')
+
+    # plt.tight_layout()
+    # plt.savefig('../lr.png', dpi=300)
+    plt.show()
+
+
+def test_6(data_file_path, resolution, *args):
+    """`avg_last_3yr_viols` vs `last_yr_viols`
+
+    """
+    data = pd.read_csv(data_file_path)
+    data.fillna(0)
+
+    feature1 = args[0]
+    feature2 = args[1]
+    label = args[2]
+
+    x_train = data[[feature1, feature2]].values
+    y_train = data[label].values
+
+    lr = LogisticRegression(C=500)
+    lr.fit(x_train, y_train)
+
+    plot_decision_regions(x_train, y_train, classifier=lr,
+                          resolution=resolution)
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.legend(loc='upper left')
+
+    # plt.tight_layout()
+    # plt.savefig('../lr.png', dpi=300)
+    plt.show()
+
+
+def test_7(data_file_path, resolution, *args):
+    """`avg_last_3yr_penals` vs `last_yr_penals`
+
+    """
+    data = pd.read_csv(data_file_path)
+    data.fillna(0)
+
+    feature1 = args[0]
+    feature2 = args[1]
+    label = args[2]
+
+    x_train = data[[feature1, feature2]].values
+    y_train = data[label].values
+
+    lr = LogisticRegression(C=500)
+    lr.fit(x_train, y_train)
+
+    plot_decision_regions(x_train, y_train, classifier=lr,
+                          resolution=resolution)
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.legend(loc='upper left')
+
+    # plt.tight_layout()
+    # plt.savefig('../lr.png', dpi=300)
+    plt.show()
+
+
+def test_8(data_file_path, resolution, *args):
+    """`log(avg_last_3yr_penals)` vs `log(last_yr_penals)`
+
+    """
+    data = pd.read_csv(data_file_path)
+    data.fillna(0)
+
+    feature1 = args[0]
+    feature2 = args[1]
+    label = args[2]
+
+    data[feature1] = np.log(data[feature1] + 1)
+    data[feature2] = np.log(data[feature2] + 1)
+    x_train = data[[feature1, feature2]].values
+    y_train = data[label].values
+
+    lr = LogisticRegression(C=500)
+    lr.fit(x_train, y_train)
+
+    plot_decision_regions(x_train, y_train, classifier=lr,
+                          resolution=resolution)
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.legend(loc='upper left')
+
+    # plt.tight_layout()
+    # plt.savefig('../lr.png', dpi=300)
+    plt.show()
+
+
 if __name__ == "__main__":
     dataset = ['../data/Recent_5_Mine_Accidents_Data_training_2.csv',
                '../data/top_5_accident_mines_processed_geq_30.csv',
-               '../data/top_5_accident_mines_processed_geq_80.csv']
+               '../data/top_5_accident_mines_processed_geq_80.csv',
+               '../data/mine-id/4608791.csv',
+               '../data/mine-id/4608436.csv',
+               '../data/mine-id/4601437.csv',
+               '../data/mine-id/4201715.csv',
+               '../data/mine-id/1202215.csv',
+               '../data/mine-id/4608791_label-geq2.csv',
+               '../data/mine-id/master.csv',
+               '../data/mine-id/4608791_rolling_quarterly.csv',
+               '../data/mine-id/4608791_quarterly.csv',
+               '../data/mine-id/4608791_yearly.csv',
+               '../Data/mine-id/master_yearly.csv']
     feature_list = ['num_violations',
                     'num_violations_t_minus_1',
-                    'total_penalties']
+                    'total_penalties',
+                    u'date',
+                    u'last_yr_viols',
+                    u'last_yr_penals',
+                    u'avg_last_3yr_viols',
+                    u'avg_last_3yr_penals',
+                    'accident_label_t_plus_1',
+                    u'accident_label']
 
-    # test_1(dataset[1], feature_list)
-    # test_2(dataset[0], feature_list)
-    # test_3(dataset[2], feature_list)
-    test_4(dataset[1], feature_list)
+    """
+    # Group 1 - pre-accident feature tests
+    test_1(dataset[1], 0.5, feature_list[0],
+           feature_list[2], feature_list[8])
+    test_2(dataset[0], 0.5, feature_list[0],
+           feature_list[1], feature_list[8])
+    test_3(dataset[2], feature_list[2], feature_list[8])
+    test_4(dataset[1], 100, feature_list[0], feature_list[8])
+    """
+
+    """
+    # Group 2 - mine-id logistic regression
+    test_5(dataset[3], 50, feature_list[4],
+           feature_list[5], feature_list[9])
+    test_6(dataset[7], 10, feature_list[4],
+           feature_list[6], feature_list[9])
+    test_6(dataset[9], 10, feature_list[4],
+           feature_list[6], feature_list[9])  # all 6 mine data in one csv
+    test_7(dataset[3], 50, feature_list[5],
+           feature_list[7], feature_list[9])
+    """
+
+    """
+    # Group 3 - different time span
+    test_6(dataset[10], 50, feature_list[4],
+           feature_list[6], feature_list[9])  # quarterly rolling
+    test_7(dataset[11], 100, feature_list[5],
+           feature_list[7], feature_list[9])  # quarterly
+    test_7(dataset[12], 100, feature_list[5],
+           feature_list[7], feature_list[9])  # yearly
+    test_6(dataset[13], 50, feature_list[4],
+           feature_list[6], feature_list[9])  # yearly all 6 mind data
+    """
+    test_8(dataset[13], 0.5, feature_list[5],
+           feature_list[7], feature_list[9])  # yearly all 6 mind data
