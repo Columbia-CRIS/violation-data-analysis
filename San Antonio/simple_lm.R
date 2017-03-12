51864382561e7144ae6374501f624e5e2176470b
l16cn <gol16cn@gmail.com>
2017-03-12 17:56:59 -0400

    presentation materials

--- San Antonio/simple_lm.R
+++ San Antonio/simple_lm.R
@@ -4,8 +4,39 @@
 
 # setup
 require(dplyr)
+require(survival)
+require(ggplot2)
 setwd("~/Git/violation-data-analysis")
 load("./San Antonio/output/result.RData")
+
+train <- complete.active.quarters
+# train <- train %>% rowwise() %>% mutate(high_severity = num.death > 0 | num.days.lost > 100)
+# train <- train[, c(1:5, 22, 6:21)]
+# train %>% count(high_severity)
+
+View(train %>% filter(mine_id == 1102752))
+new_era <- accidents %>% filter(mine_id == 1102752 & cal_qtr == 2 & cal_yr == 2005) %>% arrange(
+  desc(days_lost)
+)
+View(new_era)
+
+summary(lm(num.days.lost + num.days.restrict ~ last.quarter.viol+last.year.viol+last.three.years.viol, data=train %>% filter(active)))
+
+bad <- train %>% filter(active & num.days.restrict >= 30 & last.quarter.lost > 0)
+bad$mark <- 'bad'
+good <- train %>% filter(active & num.days.restrict < 30 & last.quarter.lost > 0)
+good$mark <- 'good'
+goodbad <- rbind(good, bad)
+goodbad <- goodbad %>% rowwise() %>% mutate(ratio = last.quarter.lost / (last.year.lost / 4))
+ggplot(goodbad, aes(num.days.lost)) + geom_density(alpha = 0.2)
+plot(ecdf(x = train$num.days.lost))
+
+# msha_glm <- clogit(high_severity ~ num.days.lost + viol.quantity + strata(mine_id),
+#                    data = train%>% top_n(100) %>% filter(active) %>% select(mine_id, num.days.lost, high_severity, viol.quantity))
+# 
+# print(msha_glm)
+
+sapply(train, function(x) sum(is.na(x)))
 
 # lm on composite num.days.lost
 summary(lm(30*num.death+1*num.days.lost+1*num.days.restrict~last.quarter.lost+last.year.lost+last.three.years.lost
