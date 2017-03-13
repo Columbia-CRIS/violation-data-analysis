1a46954efc254fd0ce18529328ee435f93af2958
l16cn <gol16cn@gmail.com>
2017-03-12 20:03:26 -0400

    added perm. dis. San Antonio result

--- San Antonio/separate_fatality.R
+++ San Antonio/separate_fatality.R
@@ -31,10 +31,12 @@ actual.accidents <- accidents %>% filter(is.element(inj_degr_desc, c("DAYS AWAY 
                                                                      "PERM TOT OR PERM PRTL DISABLTY", 
                                                                      "NO VALUE FOUND")))
 actual.accidents["deaths"] <- 0
+actual.accidents["perm_dis"] <- 0
 na.and.no.value.index <- is.element(actual.accidents$inj_degr_desc, "NO VALUE FOUND") & is.na(actual.accidents$days_lost)
 actual.accidents[na.and.no.value.index, "days_restrict"] <- 0
 actual.accidents[na.and.no.value.index, "days_lost"] <- 0
 actual.accidents[which(actual.accidents$inj_degr_desc == "FATALITY"), "deaths"] <- 1
+actual.accidents[which(actual.accidents$inj_degr_desc == "PERM TOT OR PERM PRTL DISABLTY"), "perm_dis"] <- 1
 actual.accidents$ai_dt_actual_date <- strftime(actual.accidents$ai_dt, "%F")
 rm(na.and.no.value.index, accidents)
 
@@ -45,6 +47,10 @@ days_lost_rollover <- roll_over(actual.accidents,
                                 longest.period,
                                 actual.start.year,
                                 end.year)
+colnames(days_lost_rollover) <- c(
+  "mine_id", "year", "quarter", "num.days.lost",
+  "last.quarter.lost", "last.year.lost", "last.three.years.lost"
+  )
 rm(quarter.level.num.days.lost)
 
 quarter.level.num.days.restrict <- actual.accidents %>% group_by(mine_id, cal_qtr, cal_yr)%>% 
@@ -54,7 +60,10 @@ days_restrict_rollover <- roll_over(actual.accidents,
                                 longest.period,
                                 actual.start.year,
                                 end.year)
-colnames(days_restrict_rollover)[4] <- "num.days.restrict"
+colnames(days_restrict_rollover) <- c(
+  "mine_id", "year", "quarter", 
+  "num.days.restrict", "last.quarter.restrict", "last.year.restrict", "last.three.years.restrict"
+)
 rm(quarter.level.num.days.restrict)
 
 quarter.level.num.deaths <- actual.accidents %>% group_by(mine_id, cal_qtr, cal_yr)%>% 
@@ -64,13 +73,34 @@ death_rollover <- roll_over(actual.accidents,
                                longest.period,
                                actual.start.year,
                                end.year)
-colnames(death_rollover)[4] <- "num.deaths"
+colnames(death_rollover) <- c(
+  "mine_id", "year", "quarter", 
+  "num.death", "last.quarter.death", "last.year.death", "last.three.years.death"
+)
 rm(quarter.level.num.deaths)
+
+quarter.level.num.perm_dis <- actual.accidents %>% group_by(mine_id, cal_qtr, cal_yr)%>% 
+  summarize(base = sum(perm_dis, na.rm = T))
+dis_rollover <- roll_over(actual.accidents,
+                            quarter.level.num.perm_dis,
+                            longest.period,
+                            actual.start.year,
+                            end.year)
+colnames(dis_rollover) <- c(
+  "mine_id", "year", "quarter", 
+  "num.dis", "last.quarter.dis", "last.year.dis", "last.three.years.dis"
+)
+rm(quarter.level.num.perm_dis)
 
 # cleanup violations
 active_violation_rollover = full.days_lost.accidents.date %>%
   select(mine_id, year, quarter, viol.quantity, last.quarter.y, last.year.y, last.three.years.y)
 active_violation_rollover$active <- TRUE
+colnames(active_violation_rollover) <- c(
+  "mine_id", "year", "quarter", 
+  "viol.quantity", "last.quarter.viol", "last.year.viol", "last.three.years.viol",
+  "active"
+)
 
 
 # join accidents and violation
@@ -78,23 +108,28 @@ temp <- merge(days_lost_rollover, days_restrict_rollover, by = c("mine_id","year
 temp[is.na(temp)] <- 0
 temp <- merge(temp, death_rollover, by = c("mine_id","year","quarter"),all=TRUE)
 temp[is.na(temp)] <- 0
+temp <- merge(temp, dis_rollover, by = c("mine_id","year","quarter"),all=TRUE)
+temp[is.na(temp)] <- 0
 
 temp <- merge(temp, active_violation_rollover, by = c("mine_id","year","quarter"),all.x=TRUE)
 temp[is.na(temp$active),]$active <- FALSE
 temp[is.na(temp)] <- 0
 
+colnames(mines) <- c("mine_id", "mine.name")
 temp <- merge(temp, mines, by = "mine_id", all.x=TRUE)
+
+temp <- temp[, c(1, 25, 2:3, 24, 4:23)]
 
 # result
 complete.active.quarters <- temp
-colnames(complete.active.quarters) <- 
-  c("mine_id", "year", "quarter", 
-    "num.days.lost", "last.quarter.lost", "last.year.lost", "last.three.years.lost",
-    "num.days.restrict", "last.quarter.restrict", "last.year.restrict", "last.three.years.restrict",
-    "num.death", "last.quarter.death", "last.year.death", "last.three.years.death",
-    "viol.quantity", "last.quarter.viol", "last.year.viol", "last.three.years.viol",
-    "active", "mine.name")
-complete.active.quarters <- complete.active.quarters[, c(1, 21, 2:3, 20, 4:19)]
+# colnames(complete.active.quarters) <- 
+#   c("mine_id", "year", "quarter", 
+#     "num.days.lost", "last.quarter.lost", "last.year.lost", "last.three.years.lost",
+#     "num.days.restrict", "last.quarter.restrict", "last.year.restrict", "last.three.years.restrict",
+#     "num.death", "last.quarter.death", "last.year.death", "last.three.years.death",
+#     "viol.quantity", "last.quarter.viol", "last.year.viol", "last.three.years.viol",
+#     "active", "mine.name")
+# complete.active.quarters <- complete.active.quarters[, c(1, 21, 2:3, 20, 4:19)]
 
 save(complete.active.quarters, file="./San Antonio/output/result.RData")
 
