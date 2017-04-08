07bb773235306e5b5c905e0125256afe9a11b925
l16cn <gol16cn@gmail.com>
2017-04-08 16:37:42 -0400

    Follow-up after San Antonio

---
+++ Santa Antonio/src/import_msha_txt.R
@@ -0,0 +1,70 @@
+# Setup ----
+# All data are after 2000
+rm(list=ls())
+require(dplyr)
+setwd("~/Git/violation-data-analysis") # !!CHANGE WORKING DIR TO MATCH YOUR CASE!!
+dest.dir <- "./Santa Antonio/data"
+dir.create(file.path(dest.dir))
+
+# Import violation ----
+# Date retried: 4/8/2017
+# download and import violation table
+temp <- tempfile()
+download.file(url = "https://arlweb.msha.gov/OpenGovernmentData/DataSets/AssessedViolations.zip",
+              destfile = temp)
+AssessedViolations <- read.table(file = unzip(zipfile = temp, files = "AssessedViolations.txt"), 
+                                 sep = "|", stringsAsFactors = FALSE, fill = TRUE, header = TRUE)
+unlink(temp)
+sapply(AssessedViolations, class)
+
+# download and import violation definition
+temp <- tempfile()
+download.file(url = "https://arlweb.msha.gov/OpenGovernmentData/DataSets/Assessed_Violations_Definition_File.txt",
+              destfile = temp)
+AssessedViolations.definition <- read.table(file = temp,
+                                            sep = "|", stringsAsFactors = FALSE, fill = TRUE, header = TRUE,
+                                            quote = NULL)
+unlink(temp)
+save(AssessedViolations, AssessedViolations.definition, file = file.path(dest.dir, "AssessedViolations.RData"))
+
+# Import accidents ----
+# Date retried: 4/8/2017
+# accident table
+temp <- tempfile()
+download.file(url = "https://arlweb.msha.gov/OpenGovernmentData/DataSets/Accidents.zip",
+              destfile = temp)
+Accidents <- read.table(file = unzip(zipfile = temp, files = "Accidents.txt"), 
+                        sep = "|", stringsAsFactors = FALSE, fill = TRUE, header = TRUE)
+unlink(temp)
+sapply(Accidents, class)
+
+# accident definition
+temp <- tempfile()
+download.file(url = "https://arlweb.msha.gov/OpenGovernmentData/DataSets/Accidents_Definition_File.txt",
+              destfile = temp)
+Accidents.definition <- read.table(file = temp,
+                                   sep = "|", stringsAsFactors = FALSE, fill = TRUE, header = TRUE,
+                                   quote = NULL)
+unlink(temp)
+save(Accidents, Accidents.definition, file = file.path(dest.dir, "Accidents.RData"))
+
+# Import mines ----
+# Date retried: 4/8/2017
+# mine table
+temp <- tempfile()
+download.file(url = "https://arlweb.msha.gov/OpenGovernmentData/DataSets/Mines.zip",
+              destfile = temp)
+Mines <- read.table(file = unzip(zipfile = temp, files = "Mines.txt"), 
+                    sep = "|", stringsAsFactors = FALSE, fill = TRUE, header = TRUE)
+unlink(temp)
+sapply(Mines, class)
+
+# mine definition
+temp <- tempfile()
+download.file(url = "https://arlweb.msha.gov/OpenGovernmentData/DataSets/Mines_Definition_File.txt",
+              destfile = temp)
+Mines.definition <- read.table(file = temp,
+                               sep = "|", stringsAsFactors = FALSE, fill = TRUE, header = TRUE,
+                               quote = NULL)
+unlink(temp)
+save(Mines, Mines.definition, file = file.path(dest.dir, "Mines.RData"))
